91221b664824fe27731c0915399b483a8c8b49df
Sammy-Geeeee <Sam.Givan@outlook.com>
2024-10-23 19:31:57 +0800

    Updating commit, no significant changes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Calculus Plotter
 
-# This program is to provide an interface to do some basic calculus functions within
-# It will find derivatives, integrals, the area between curves, lengths of curves, etc...
-# It will also plot all this so that you can see an illustrated version of what is being done
+"""
+This program is to provide an interface to do some basic calculus functions within
+It will find derivatives, integrals, the area between curves, lengths of curves, etc...
+It will also plot all this so that you can see an illustrated version of what is being done
+
+This program was made in attempt to consolidate a bunch of tools I frequently had to go looking for during uni
+"""
 
 
 from frameSingle import *
